4f479fbbd5341b3300200b4f1c80a841824fedf6
jefferson-bit <jhonsonbabi@hotmail.com>
2020-10-22 17:41:22 -0300

    implementing OAuth2 authorizaton server

--- backend/src/main/java/com/crash/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/crash/dscatalog/repositories/UserRepository.java
@@ -7,7 +7,5 @@ import com.crash.dscatalog.domains.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-	
 	User findByEmail(String email);
 }
--- backend/src/main/java/com/crash/dscatalog/services/UserService.java
+++ backend/src/main/java/com/crash/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +30,10 @@ import com.crash.dscatalog.services.exceptions.DataBaseException;
 import com.crash.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
-
+public class UserService implements UserDetailsService{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private BCryptPasswordEncoder bp;	
 	
@@ -89,5 +96,16 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if(user ==  null) {
+			LOG.error("User not found: " + username);
+			throw new UsernameNotFoundException("email not found");
+		}
+		LOG.info("User found: " + username);
+		return user;
 	}
 }
